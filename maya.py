f3fe9669d7f9774484681e10dc136fdd7db2e5ee
Ezra Estud <ezra.estud740@escapestudios.ac.uk>
2025-10-20 21:04:41 +0100

    Added a code that allows the user to rename the Geo's original name. I also tried working on the Ui layout but it still needs improvement. Next part is adding a function that can delete any names with "__pasted"

--- maya.py
+++ maya.py
@@ -1,36 +1,68 @@
 import maya.cmds as cmds
 
+#Don't use Caps at start for some reason
 windowName = 'Simple Renamer Tool'
 
-if cmds.window(windowName, ex=True):
+if cmds.window(windowName, exists=True):
     print('exists')
-    cmds.deleteUI(windowName, window=True)
+    cmds.deleteUI(windowName)
+    cmds.windowPref(windowName, remove=True)
 
-window = cmds.window(windowName)
-cmds.columnLayout(adjustableColumn=True, rowSpacing=10)
+    
+
+window = cmds.window(windowName, resizeToFitChildren=True, sizeable=True, widthHeight=(180,200))
+cmds.columnLayout(adjustableColumn=True, rowSpacing=100)
 form = cmds.formLayout()
-row = cmds.rowLayout(numberOfColumns=2, columnWidth=(80, 200), columnAlign=(1, "left"))
-cmds.formLayout(form, edit = True, attachForm = [(row, 'top', 0)])
+
+
+row1 = cmds.rowLayout(numberOfColumns=2, columnWidth=(120, 200), adjustableColumn=2)
+cmds.formLayout(form, edit = True, attachForm = [(row1, 'top', 10)])
 
 #Finding the Name
 def findName(*args):
     #print(f"text field = {ObjFind}")
-    ObjName = cmds.textField(ObjFind, query=True, text=True)
+    ObjName = cmds.textField(ObjFind, query=True, text=True).lower()
     #print(f"input = {ObjName}")
-    matched = cmds.ls(f"*{ObjName}*", type = 'transform')
+    allObj = cmds.ls(type = 'transform')
+    matched = [obj for obj in allObj if ObjName in obj.lower() and obj not in ['persp', 'top', 'front', 'side'] and not cmds.lockNode(obj,query=True, lock=True)[0]]
     #print(matched)
     if matched:
         cmds.select(matched)
     else:
         cmds.warning(f"Object '{ObjName}' does not exist")
+    cmds.textField(ObjFind, edit=True, text="")
     
     
 cmds.button("Find all named:", command=findName)
-ObjFind = cmds.textField()
+ObjFind = cmds.textField(w=130)
+cmds.setParent('..')
 
-#Replacing the Name
+
+
+row2 = cmds.rowLayout(numberOfColumns=2, columnWidth=(160, 200), adjustableColumn=2)
+cmds.formLayout(form, edit = True, attachControl = [(row2,'top', 10, row1)], attachForm =[(row2, 'left', 0)])
+
+#Replacing the Namem
 def rename(*args):
-    cmds.button("Replace selected with:", command=rename)
+    ObjRename = cmds.textField(ObjNewName, query=True, text=True)
+    selected = cmds.ls(selection=True)
+    if not selected:
+        cmds.warning("No Objects to be renamed")
+        return
+    if not ObjRename.strip():
+        cmds.warning("Enter a new name")
+        return
+    for i, obj in enumerate(selected, start=1):
+        if len(selected) == 1:
+            newName = cmds.rename(obj, ObjRename)
+        else:
+            newName = cmds.rename(obj, f"{ObjRename}_{i:02d}")
+            print(f"Renamed: {obj} to {newName}")
+    cmds.textField(ObjNewName, edit=True, text="")
+
+cmds.button("Replace Selected with:", command=rename)
+ObjNewName = cmds.textField()
+cmds.setParent('..')
     
 
 
