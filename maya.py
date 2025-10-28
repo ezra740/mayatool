54e04a0aa436cce66361300ed9cfb1e47d393cdc
ezra740 <ezra.estud740@escapestudios.ac.uk>
2025-10-28 18:06:09 +0000

    Little change. Added so that a drop box can select only GEOs. What I am planning is, making a function that selects types of lights, meshes, joints etc.

--- maya.py
+++ maya.py
@@ -15,8 +15,8 @@ cmds.columnLayout(adjustableColumn=False, rowSpacing=100)
 form = cmds.formLayout()
 
 
-row1 = cmds.rowLayout(numberOfColumns=2, columnWidth=(120, 200), adjustableColumn=2)
-cmds.formLayout(form, edit = True, attachForm = [(row1, 'top', 10)])
+row1 = cmds.rowLayout(numberOfColumns=3, columnWidth=(120, 200), adjustableColumn=2)
+cmds.formLayout(form, edit = True, attachForm = [(row1, 'top', 10), (row1, 'left', 15)])
 
 #Finding the Name
 def findName(*args):
@@ -28,18 +28,18 @@ def findName(*args):
     #print(matched)
     if matched:
         cmds.select(matched)
-    else:
-        cmds.warning(f"Object '{ObjName}' does not exist")
-    cmds.textField(ObjFind, edit=True, text="")
+        cmds.textField(ObjFind, edit=True, text="")
 
-cmds.button("Find all named:", command=findName, w=125)
+cmds.button("Find all name:", command=findName, w=125)
 ObjFind = cmds.textField(w=150)
+
+
 cmds.setParent('..')
 
 
 
 row2 = cmds.rowLayout(numberOfColumns=2, columnWidth=(160, 200), adjustableColumn=2)
-cmds.formLayout(form, edit = True, attachControl = [(row2,'top', 10, row1)], attachForm =[(row2, 'left', 0)])
+cmds.formLayout(form, edit = True, attachControl = [(row2,'top', 10, row1)], attachForm =[(row2, 'left', 15)])
 
 #Replacing the Name
 def rename(*args):
@@ -65,7 +65,7 @@ cmds.setParent('..')
     
 
 row3 = cmds.rowLayout(numberOfColumns=1, columnWidth=(160, 200), adjustableColumn=2)
-cmds.formLayout(form, edit = True, attachControl = [(row3,'top', 45, row1)], attachForm =[(row3, 'left', 0)])
+cmds.formLayout(form, edit = True, attachControl = [(row3,'top', 10, row2)], attachForm =[(row3, 'left', 8)])
 
 #Deleting the "pasted__"
 def deletePaste(*args):
@@ -77,15 +77,13 @@ def deletePaste(*args):
         cmds.rename(obj, clearPaste)
         print("Removed all pasted__")
 
-cmds.button("Remove 'pasted__'", w=400, command=deletePaste)
+cmds.button("Remove 'pasted__'", w=445, command=deletePaste, h= 35)
 cmds.setParent('..')
-
-cmds.showWindow()
 
 
 #Adding Prefixes and Suffixes
 row4 = cmds.rowLayout(numberOfColumns=5, columnWidth=(160, 200), adjustableColumn=2)
-cmds.formLayout(form, edit = True, attachControl = [(row4,'top', 80, row1)], attachForm =[(row4, 'left', 0)])
+cmds.formLayout(form, edit = True, attachControl = [(row4,'top', 10, row3)], attachForm =[(row4, 'left', 25)])
 cmds.button(label="Add prefix__:", command=lambda *args:addPrefix())
 prefixBox = cmds.textField()
 cmds.text(label="(Object)", w=50, backgroundColor=(0.3,0.3,0.3), font="boldLabelFont", ann="Your Selected Object")
@@ -107,11 +105,26 @@ def addSuffix(*args):
         cmds.rename(obj, f"{obj}_{suffix}")
     cmds.textField(suffixBox, edit=True, text="")
 
+#Removes all custom prefix and suffixes and only keeps core name
+SubRow = cmds.rowLayout(numberOfColumns=5, columnWidth=(160, 200), adjustableColumn=2)
+cmds.formLayout(form, edit = True, attachControl = [(SubRow,'top', 15, row4)], attachForm =[(SubRow, 'left', 25)])
+
+def clearPrefixSuffix(*args):
+    selected = cmds.ls('_*', type = 'transform')
+    if selected:
+        cmds.select(selected)
+        for obj in selected:
+            clearPrefix = obj.replace('_*', '', 1)
+            cmds.rename(obj, clearPrefix)
+
+
+cmds.button('Remove all Prefixes and Suffixes', w=400, h=30, command = clearPrefixSuffix)
+cmds.setParent('..')
 
 #Adding a selector for object type and sub-types
 
 row5 = cmds.rowLayout(numberOfColumns=3, columnWidth=(160, 200), adjustableColumn=True)
-cmds.formLayout(form, edit = True, attachControl = [(row5,'top', 115, row1)], attachForm =[(row5, 'left', 0)])
+cmds.formLayout(form, edit = True, attachControl = [(row5,'top', 60, row4)], attachForm =[(row5, 'left', 10)])
 
 #Option 1 would be geometry & sub_types
 def option1(selection):
@@ -173,7 +186,7 @@ cmds.setParent('..')
 
 #Auto Apply Identity Suffixes 
 row6 = cmds.rowLayout(numberOfColumns=3, columnWidth=(160, 200), adjustableColumn=True)
-cmds.formLayout(form, edit = True, attachControl = [(row6,'top', 155, row1)], attachForm =[(row6, 'left', 0)])
+cmds.formLayout(form, edit = True, attachControl = [(row6,'top', 10, row5)], attachForm =[(row6, 'left', 18)])
 
 def autoSuffix(*args):
     geo = cmds.ls(type='mesh')
@@ -238,6 +251,157 @@ cmds.button("Add Naming Type Suffixes",command=autoSuffix, w=200, h=50, ann= "Ad
 cmds.separator(width=5, style='none')
 cmds.button("Remove Naming Type Suffixes", command = removeSuffix, w=200, h=50, ann= "Removes all Naming Conventions Type")
 cmds.setParent('..')
+
+
+#Adding Positions Macros for Models
+
+row7 = cmds.rowLayout(numberOfColumns=2, columnWidth=(160, 200), adjustableColumn=True)
+cmds.formLayout(form, edit = True, attachControl = [(row7,'top', 10, row6)], attachForm =[(row7, 'left', 200)])
+
+#Uses of class
+class prefixButton():
+    def __init__(self, label):
+        self.prefix = label
+        self.name = cmds.button(label, w=50, h=50, command = self.addPrefix)
+
+    def addPrefix(self, *args):
+        selected = cmds.ls(selection=True)
+        for obj in selected:
+            if not obj.startswith(self.prefix):
+                addLeft = f"{self.prefix}{obj}"
+                cmds.rename(obj, addLeft)
+
+prefixButton('top_')
+cmds.setParent('..')
+
+row8 = cmds.rowLayout(numberOfColumns=11, columnWidth=(160, 200), adjustableColumn=True)
+cmds.formLayout(form, edit = True, attachControl = [(row8,'top', 5, row7)], attachForm =[(row8, 'left', 200)])
+
+
+
+prefixButton('upp_')
+cmds.setParent('..')
+
+row9 = cmds.rowLayout(numberOfColumns=15, columnWidth=(160, 200), adjustableColumn=True)
+cmds.formLayout(form, edit = True, attachControl = [(row9,'top', 5, row8)], attachForm =[(row9, 'left', 23)])
+
+prefixButton('L_')
+cmds.separator(width=5, style='none')
+prefixButton('fnt_')
+cmds.separator(width=5, style='none')
+prefixButton('int_')
+cmds.separator(width=5, style='none')
+prefixButton('cntr_')
+cmds.separator(width=5, style='none')
+prefixButton('ext_')
+cmds.separator(width=5, style='none')
+prefixButton('bck_')
+cmds.separator(width=5, style='none')
+prefixButton('R_')
+cmds.setParent('..')
+
+
+row10 = cmds.rowLayout(numberOfColumns=11, columnWidth=(160, 200), adjustableColumn=True)
+cmds.formLayout(form, edit = True, attachControl = [(row10,'top', 5, row9)], attachForm =[(row10, 'left', 200)])
+
+prefixButton('lwr_')
+cmds.setParent('..')
+
+row11 = cmds.rowLayout(numberOfColumns=2, columnWidth=(160, 200), adjustableColumn=True)
+cmds.formLayout(form, edit = True, attachControl = [(row11,'top', 5, row10)], attachForm =[(row11, 'left', 200)])
+
+prefixButton('btm_')
+
+def removePosSuffix(*args):
+
+    top = cmds.ls('*top_*', type= 'transform')
+    if top:
+        cmds.select(top)
+        for obj in top:
+            clearTop = obj.replace('top_', '', 1)
+            cmds.rename(obj, clearTop)
+
+    upper = cmds.ls('*upp_*', type= 'transform')
+    if upper:
+        cmds.select(upper)
+        for obj in upper:
+            clearUpper = obj.replace('upp_', '', 1)
+            cmds.rename(obj, clearUpper)
+
+    left = cmds.ls('*L_*', type='transform')
+    if left:
+        cmds.select(left)
+    for obj in left:
+        clearLeft = obj.replace('L_', '', 1)
+        cmds.rename(obj, clearLeft)
+    
+    front = cmds.ls('*fnt_*', type= 'transform')
+    if front:
+        cmds.select(front)
+        for obj in front:
+            clearFront = obj.replace('fnt_', '', 1)
+            cmds.rename(obj, clearFront)
+    
+    interior = cmds.ls('*int_*', type= 'transform')
+    if interior:
+        cmds.select(interior)
+        for obj in interior:
+            clearInt = obj.replace('int_', '', 1)
+            cmds.rename(obj, clearInt)
+    
+    center = cmds.ls('*cntr_*', type= 'transform')
+    if center:
+        cmds.select(center)
+        for obj in center:
+            clearCent = obj.replace('cntr_', '', 1)
+            cmds.rename(obj, clearCent)
+
+    exterior = cmds.ls('*ext_*', type= 'transform')
+    if exterior:
+        cmds.select(exterior)
+        for obj in exterior:
+            clearExt = obj.replace('ext_', '', 1)
+            cmds.rename(obj, clearExt)
+
+    back = cmds.ls('*bck_*', type= 'transform')
+    if back:
+        cmds.select(back)
+        for obj in back:
+            clearBack = obj.replace('bck_', '', 1)
+            cmds.rename(obj, clearBack)
+
+    right = cmds.ls('*R_*', type= 'transform')
+    if right:
+        cmds.select(right)
+        for obj in right:
+            clearRight = obj.replace('R_', '', 1)
+            cmds.rename(obj, clearRight)
+
+    lower = cmds.ls('*lwr_*', type= 'transform')
+    if lower:
+        cmds.select(lower)
+        for obj in lower:
+            clearLower = obj.replace('lwr_', '', 1)
+            cmds.rename(obj, clearLower)
+
+    bottom = cmds.ls('*btm_*', type= 'transform')
+    if bottom:
+        cmds.select(bottom)
+        for obj in bottom:
+            clearBottom = obj.replace('btm_', '', 1)
+            cmds.rename(obj, clearBottom)
+
+cmds.setParent('..')
+
+subRow11 = cmds.rowLayout(numberOfColumns=2, columnWidth=(160, 200), adjustableColumn=True)
+cmds.formLayout(form, edit = True, attachControl = [(subRow11,'top', 30, row6)], attachForm =[(subRow11, 'left', 25)])
+
+cmds.button('Delete All Position Prefixes', command = removePosSuffix, w= 155, h= 55)
+cmds.setParent('..')
+
+
+
+
 
 cmds.showWindow()
 
